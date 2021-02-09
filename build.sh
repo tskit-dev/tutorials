2bdf7160c66ba7c06f52193808fa69f086b94bb2
Jerome Kelleher <jk@well.ox.ac.uk>
2021-02-09 13:27:06 +0000

    Tweak build script to be more robust.

--- build.sh
+++ build.sh
@@ -5,13 +5,14 @@
 # build failures in CI. This is a simple wrapper to handle that.
 
 REPORTDIR=_build/html/reports
-# Clear out any old reports
-rm -f $REPORTDIR/*
 
 jupyter-book build -W -n --keep-going .
 RETVAL=$?
 if [ $RETVAL -ne 0 ]; then
     echo "Error occured; showing saved reports"
     cat $REPORTDIR/*
+else
+    # Clear out any old reports
+    rm -f $REPORTDIR/*
 fi
 exit $RETVAL
