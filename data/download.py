7a601bbc6608dccb9523ade0f9e50566381a2887
Yan Wong <yan.wong@bdi.ox.ac.uk>
2021-07-30 20:44:09 +0100

    Fix imports

--- data/download.py
+++ data/download.py
@@ -5,13 +5,13 @@ saving to a local `data` directory
 
 import os
 import json
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, urlopen
 
 if not os.path.isdir("data"):
     os.mkdir("data")  # Make a "data" directory within the current folder
 print(f"Downloading data files into {os.path.join(os.getcwd(), 'data')}")
 # Save the data files to the data directory
-response = urllib.request.urlopen("https://tskit.dev/tutorials/examples/files.txt")
+response = urlopen("https://tskit.dev/tutorials/examples/files.txt")
 for fn in response:
     fn = fn.decode(response.headers.get_content_charset()).strip()
     if fn.endswith(".trees"):
